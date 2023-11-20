e7b346b93f204b0dbb2d1a80c520849021e32c69
tim2004timi <tim2004timi@mail.ru>
2023-11-20 22:45:04 +0300

    Add css html for index.html

--- fulfillmentapp/admin.py
+++ fulfillmentapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from.models import Seller, Product
+from.models import Seller, Product, Delivery, Operator
 
 
 admin.site.register(Seller)
 admin.site.register(Product)
+admin.site.register(Delivery)
+admin.site.register(Operator)
--- fulfillmentapp/models.py
+++ fulfillmentapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Seller(models.Model):
@@ -8,7 +9,6 @@ class Seller(models.Model):
     password = models.CharField(max_length=20)
     email = models.EmailField(blank=True, default=None)
     telegram = models.CharField(max_length=30, blank=True, default=None)
-
     time_signup = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
@@ -17,14 +17,38 @@ class Seller(models.Model):
         return self.login
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField(blank=True, default=None)
-    owner = models.ForeignKey("Seller", on_delete=models.CASCADE)
+class Operator(models.Model):
+    login = models.CharField(max_length=20, primary_key=True, unique=True)
+    password = models.CharField(max_length=20)
 
-    time_create = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.login
+
+
+class Product(models.Model):
+    article = models.IntegerField(validators=[MinValueValidator(10000)], default=10001, primary_key=True, unique=True)
+    name = models.CharField(default=None, max_length=30)
+    size = models.CharField(default=None, max_length=30)
+    color = models.CharField(default=None, max_length=30)
+    numbers = models.IntegerField(default=1, validators=[MaxValueValidator(100)])
+    seller = models.ForeignKey("Seller", on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, default=None, blank=True)
 
     objects = models.Manager()
 
     def __str__(self):
         return self.name
+
+
+class Delivery(models.Model):
+    product = models.OneToOneField("Product", on_delete=models.CASCADE, primary_key=True)
+    address = models.TextField()
+    driver_fio = models.CharField(max_length=50)
+    car_number = models.CharField(max_length=20)
+    date = models.DateField()
+    qr = models.ImageField()
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"{self.product.name} отгрузка"
