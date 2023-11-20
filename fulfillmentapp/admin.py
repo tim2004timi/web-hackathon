33e391359803e59ada0376ccb49994e7306eb9a0
tim2004timi <tim2004timi@mail.ru>
2023-11-21 00:01:08 +0300

    Fix some visual

--- fulfillmentapp/admin.py
+++ fulfillmentapp/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from.models import Seller, Product, Delivery, Operator
+from .models import Seller, Product, Delivery, Operator
 
 
-admin.site.register(Seller)
-admin.site.register(Product)
-admin.site.register(Delivery)
-admin.site.register(Operator)
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ("name", "last_name", "login", "password", "email", "telegram", "time_signup")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("article", "name", "size", "color", "numbers", "seller", "status", "delivery")
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ("product", "address", "date", "driver_fio", "qr")
+
+
+@admin.register(Operator)
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ("login", "password")
--- fulfillmentapp/models.py
+++ fulfillmentapp/models.py
@@ -3,12 +3,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Seller(models.Model):
-    name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    login = models.CharField(max_length=20, primary_key=True, unique=True)
-    password = models.CharField(max_length=20)
-    email = models.EmailField(blank=True, default=None)
-    telegram = models.CharField(max_length=30, blank=True, default=None)
+    name = models.CharField(max_length=30, verbose_name="Имя")
+    last_name = models.CharField(max_length=30, verbose_name="Фамилия")
+    login = models.CharField(max_length=20, primary_key=True, unique=True, verbose_name="Логин")
+    password = models.CharField(max_length=20, verbose_name="Пароль")
+    email = models.EmailField(blank=True, default=None, verbose_name="Почта")
+    telegram = models.CharField(max_length=30, blank=True, default=None, verbose_name="Телеграм")
     time_signup = models.DateTimeField(auto_now_add=True)
 
     objects = models.Manager()
@@ -16,39 +16,57 @@ class Seller(models.Model):
     def __str__(self):
         return self.login
 
+    class Meta:
+        verbose_name = "Продавец"
+        verbose_name_plural = "Продавцы"
+
 
 class Operator(models.Model):
-    login = models.CharField(max_length=20, primary_key=True, unique=True)
-    password = models.CharField(max_length=20)
+    login = models.CharField(max_length=20, primary_key=True, unique=True, verbose_name="Логин")
+    password = models.CharField(max_length=20, verbose_name="Пароль")
+
+    objects = models.Manager()
 
     def __str__(self):
         return self.login
 
+    class Meta:
+        verbose_name = "Оператор"
+        verbose_name_plural = "Операторы"
+
 
 class Product(models.Model):
-    article = models.IntegerField(validators=[MinValueValidator(10000)], default=10001, primary_key=True, unique=True)
-    name = models.CharField(default=None, max_length=30)
-    size = models.CharField(default=None, max_length=30)
-    color = models.CharField(default=None, max_length=30)
-    numbers = models.IntegerField(default=1, validators=[MaxValueValidator(100)])
-    seller = models.ForeignKey("Seller", on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, default=None, blank=True)
+    article = models.AutoField(primary_key=True, verbose_name="Артикль")
+    name = models.CharField(default=None, max_length=30, verbose_name="Название")
+    size = models.CharField(default=None, max_length=30, verbose_name="Размер (20х20х30)")
+    color = models.CharField(default=None, max_length=30, verbose_name="Цвет")
+    numbers = models.IntegerField(default=1, validators=[MaxValueValidator(100)], verbose_name="Кол-во")
+    seller = models.ForeignKey("Seller", on_delete=models.CASCADE, verbose_name="Продавец")
+    status = models.CharField(max_length=20, default=None, blank=True, verbose_name="Статус")
 
     objects = models.Manager()
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
 
 class Delivery(models.Model):
-    product = models.OneToOneField("Product", on_delete=models.CASCADE, primary_key=True)
-    address = models.TextField()
-    driver_fio = models.CharField(max_length=50)
-    car_number = models.CharField(max_length=20)
-    date = models.DateField()
-    qr = models.ImageField()
+    product = models.OneToOneField("Product", on_delete=models.CASCADE, primary_key=True, verbose_name="Товар")
+    address = models.TextField(verbose_name="Адрес")
+    driver_fio = models.CharField(max_length=50, verbose_name="ФИО водителя")
+    car_number = models.CharField(max_length=20, verbose_name="Номер авто")
+    date = models.DateField(verbose_name="Дата")
+    qr = models.ImageField(verbose_name="QR")
 
     objects = models.Manager()
 
     def __str__(self):
-        return f"{self.product.name} отгрузка"
+        return f"Отгрузка ({self.product.name})"
+
+    class Meta:
+        verbose_name = "Отгрузка"
+        verbose_name_plural = "Отгрузки"
