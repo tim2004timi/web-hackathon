8a645e70b0351cbcdb26f9f6145ad4bcb7ffa9db
Arklim <akella44_240683@mail.ru>
2023-11-23 23:35:13 +0300

    Bot auth

--- fulfillmentapp/management/commands/bot.py
+++ fulfillmentapp/management/commands/bot.py
@@ -4,17 +4,35 @@ from telegram.ext import (
     Application,
     filters,
     ContextTypes,
-    MessageHandler)
+    MessageHandler,
+    CommandHandler)
 from telegram import Update
+from fulfillmentapp.models import Seller
+from asgiref.sync import sync_to_async
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+@sync_to_async
+def get_user_by_telegram(tg_usename: str) -> Seller:
+    return Seller.objects.filter(telegram=tg_usename).first()
+
+@sync_to_async
+def set_telegram_chat_id(user: Seller, chat_id: int) -> None:
+    user.telegram_chat_id = chat_id
+    user.save()
+
+async def auth(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = update.message
+    tg_usename = message.from_user.name
+    user = await get_user_by_telegram(tg_usename=tg_usename)
+    
+    if user:
+        await set_telegram_chat_id(user=user, chat_id=message.chat_id)
+        await message.reply_text(f"Теперь Вы можете получать уведомления в этот чат!")
+    else:
+        await message.reply_text("Вы не зарегистрированы в системе!")
 
 class Command(BaseCommand):
-    help = "tg bot"
-    
     def handle(self, *args, **options):
         application = Application.builder().token(settings.TOKEN).build()
-        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+        application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auth))
+        application.add_handler(CommandHandler("start", auth))
         application.run_polling(allowed_updates=Update.ALL_TYPES)
