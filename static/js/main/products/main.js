4f6a892298fcad9056aab10b3db41477f06a4f30
Sergey Kruchinkin <kruchinkin.s@yandex.ru>
2023-11-25 23:51:15 +0300

    add overlay and popup

--- static/js/main/products/main.js
+++ static/js/main/products/main.js
@@ -1,6 +1,24 @@
 const cabinetBtn = document.querySelector('.cabinet__btn');
 const cabinetPopup = document.querySelector('.cabinet__popup');
+const cabinetPopupWrapper = document.querySelector('.cabinet__popup-wrapper');
+const bodyEl = document.body;
 
-cabinetBtn.addEventListener('click', ()=>{
-    cabinetPopup.classList.toggle('cabinet__popup-open');
+cabinetBtn.addEventListener('click', (e)=>{
+    e.stopPropagation();
+    cabinetPopup.classList.add('cabinet__popup-open');
+    bodyEl.classList.add('cabinet__overlay');
+    cabinetPopupWrapper.classList.add('cabinet__overlay');
 });
+
+document.addEventListener('click', (e)=>{
+    const target = e.target;
+    const itsCabinetPopupWrapper = target == cabinetPopupWrapper || cabinetPopupWrapper.contains(target);
+    const itsCabinetBtn = target == cabinetBtn;
+    const overlayIsActive = cabinetPopupWrapper.classList.contains("cabinet__overlay");
+
+    if (!itsCabinetPopupWrapper && !itsCabinetBtn && overlayIsActive) {
+        cabinetPopup.classList.remove('cabinet__popup-open');
+        bodyEl.classList.remove('cabinet__overlay');
+        cabinetPopupWrapper.classList.remove('cabinet__overlay');
+    }
+})
