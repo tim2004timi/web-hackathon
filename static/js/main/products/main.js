43c93d82fd408bb2b2cc55e3a589be64d61e6dc5
Sergey Kruchinkin <kruchinkin.s@yandex.ru>
2023-11-26 19:27:02 +0300

    update products.html

--- static/js/main/products/main.js
+++ static/js/main/products/main.js
@@ -2,9 +2,14 @@ const cabinetBtn = document.querySelector('.cabinet__btn');
 const cabinetPopup = document.querySelector('.cabinet__popup');
 const cabinetPopupWrapper = document.querySelector('.cabinet__popup-wrapper');
 const bodyEl = document.body;
+const productAddWrapper = document.querySelector('.product-add__wrapper');
+const productBtn = document.querySelector('.product__btn');
+const productAddContainer = document.querySelector('.product-add__container');
+const backBtn = document.querySelector('.back-btn');
+
 
 cabinetBtn.addEventListener('click', (e)=>{
-    e.stopPropagation();
+    // e.stopPropagation();
     cabinetPopup.classList.add('cabinet__popup-open');
     bodyEl.classList.add('cabinet__overlay');
     cabinetPopupWrapper.classList.add('cabinet__overlay');
@@ -22,3 +27,14 @@ document.addEventListener('click', (e)=>{
         cabinetPopupWrapper.classList.remove('cabinet__overlay');
     }
 })
+
+productBtn.addEventListener('click', ()=>{
+    productAddWrapper.classList.add('product-add__overlay');
+    productAddContainer.classList.add('product-add__show');
+});
+
+backBtn.addEventListener('click', ()=>{
+    productAddWrapper.classList.remove('product-add__overlay');
+    productAddContainer.classList.remove('product-add__show');
+});
+
