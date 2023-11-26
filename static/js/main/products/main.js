0b54aba69a756d2b0cb867a7b6f1db3dcb34f226
tim2004timi <tim2004timi@mail.ru>
2023-11-26 22:14:01 +0300

    Fix deleting data

--- static/js/main/products/main.js
+++ static/js/main/products/main.js
@@ -1,31 +1,21 @@
 const cabinetBtn = document.querySelector('.cabinet__btn');
 const cabinetPopup = document.querySelector('.cabinet__popup');
 const cabinetPopupWrapper = document.querySelector('.cabinet__popup-wrapper');
-const bodyEl = document.body;
 const productAddWrapper = document.querySelector('.product-add__wrapper');
 const productBtn = document.querySelector('.product__btn');
 const productAddContainer = document.querySelector('.product-add__container');
 const backBtn = document.querySelector('.back-btn');
+const backBtnCabinet = document.querySelector('.back-btn-cabinet');
 
 
 cabinetBtn.addEventListener('click', (e)=>{
-    // e.stopPropagation();
+    e.stopPropagation();
     cabinetPopup.classList.add('cabinet__popup-open');
-    bodyEl.classList.add('cabinet__overlay');
     cabinetPopupWrapper.classList.add('cabinet__overlay');
 });
 
-document.addEventListener('click', (e)=>{
-    const target = e.target;
-    const itsCabinetPopupWrapper = target == cabinetPopupWrapper || cabinetPopupWrapper.contains(target);
-    const itsCabinetBtn = target == cabinetBtn;
-    const overlayIsActive = cabinetPopupWrapper.classList.contains("cabinet__overlay");
-
-    if (!itsCabinetPopupWrapper && !itsCabinetBtn && overlayIsActive) {
-        cabinetPopup.classList.remove('cabinet__popup-open');
-        bodyEl.classList.remove('cabinet__overlay');
-        cabinetPopupWrapper.classList.remove('cabinet__overlay');
-    }
+backBtnCabinet.addEventListener('click', ()=>{
+    cabinetPopupWrapper.classList.remove('cabinet__overlay');
 })
 
 productBtn.addEventListener('click', ()=>{
